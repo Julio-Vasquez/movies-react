2a55fcdb8185e4d2d3de3ab91512c1d92d04dd7b
DarKPhuRioN <jualvalitube@gmail.com>
2020-02-12 12:08:24 -0500

    basic routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React from "react";
+import { Layout } from "antd";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+//Views
+import Home from "./views/Home/Home";
+import NewMovie from "./views/NewMovie/NewMovie";
+import PopularMovies from "./views/PopularMovies/PopularMovies";
+import SearchMovies from "./views/SearchMovies/SearchMovies";
+import Movie from "./views/Movie/Movie";
+import Error404 from "./views/Error404/Error404";
 
 export default function App() {
+  const { Header, Content } = Layout;
   return (
-    <div className="App">
-      <p>hola</p>
-    </div>
+    <Layout>
+      <Router>
+        <Header>header</Header>
+        <Content>
+          <Switch>
+            <Route path="/" exact={true} component={Home} />
+            <Route path="/new-movies" exact={true} component={NewMovie} />
+            <Route path="/popular" exact={true} component={PopularMovies} />
+            <Route path="/search" exact={true} component={SearchMovies} />
+            <Route path="/movie/:id" component={Movie} exact={true} />
+            <Route path="*" component={Error404} />
+          </Switch>
+        </Content>
+      </Router>
+    </Layout>
   );
 }
--- src/views/SearchMovies/SearchMovies.js
+++ src/views/SearchMovies/SearchMovies.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function SearchMovies() {
   return (
     <div>
-      <p>Search Movies</p>
+      <p>SearchMovies</p>
     </div>
   );
 }
