5646c7057d11dd143013f1645a72cf4cf6a28d97
DarKPhuRioN <jualvalitube@gmail.com>
2020-05-24 21:15:52 -0500

    finish

--- src/views/Error404/Error404.js
+++ src/views/Error404/Error404.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import "./Error404.scss";
 
 export default function Error404() {
   return (
-    <div>
-      <p>404</p>
+    <div className="error404">
+      <h1>Error404</h1>
+      <h2>Página no encontrada</h2>
+      <Link to="/">
+        <h3>Volver al inicio</h3>
+      </Link>
     </div>
   );
 }
